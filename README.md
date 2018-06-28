8ec112e6a11ce9dc1439a11e266a24d590e2b6a5
Marek Bejda <marek.bejda@ibm.com>
2018-06-27 22:50:38 -0500

    e2e for creating invoice

--- README.md
+++ README.md
@@ -11,3 +11,16 @@ $ ./fabric.sh up
 $ ./fabric.sh startCC ccname v1
 $ ./fabric.sh runCC ccname v1
 ```
+
+Create/Query Invoice E2E
+```
+#!/usr/bin/env bash
+
+export FILE=/Users/marek.bejda/go/src/github.com/FabricBros/chaincode/fixtures/invoice_ex1.json
+export CMD=`jq --argfile file $FILE '.Args[1] = ($file | tojson)' input.json | tr -d " \t\n\r"`
+#jq --argfile file $FILE '.Records[0].Sns.Message = ($file | tojson)' input.json
+
+
+./fabric.sh invoke defaultcc v1 $CMD
+./fabric.sh query defaultcc v1 '{"Args":["RetrieveInvoice", "IN3201"]}'
+```
--- tests/e2e.sh
+++ tests/e2e.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-./fabric.sh invoke -c '{"Args":["AddInvoices", "`cat ./fixtures/invoice_ex1.json`"]}'
-./fabric.sh query -c  '{"Args":["RetrieveInvoice", "IN3201"]}'
+export FILE=/Users/marek.bejda/go/src/github.com/FabricBros/chaincode/fixtures/invoice_ex1.json
+export CMD=`jq --argfile file $FILE '.Args[1] = ($file | tojson)' input.json | tr -d " \t\n\r"`
+#jq --argfile file $FILE '.Records[0].Sns.Message = ($file | tojson)' input.json
+
+
+./fabric.sh invoke defaultcc v1 $CMD
+./fabric.sh query defaultcc v1 '{"Args":["RetrieveInvoice", "IN3201"]}'
